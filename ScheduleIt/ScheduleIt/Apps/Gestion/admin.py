428f7390d99ac44cebd6076781758b4e3fd26dde
ALEZAGI3408 <133360368+ALEZAGI3408@users.noreply.github.com>
2024-03-03 21:55:28 -0500

    Add files via upload

---
+++ ScheduleIt/ScheduleIt/Apps/Gestion/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from ScheduleIt.Apps.Gestion.models import *
+
+# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Materia)
+admin.site.register(PactoPedagogico)
+admin.site.register(Evaluacion)
