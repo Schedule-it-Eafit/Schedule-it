e503b95934cdca1205a28f53b840951d94947b87
ALEZAGI3408 <133360368+ALEZAGI3408@users.noreply.github.com>
2024-04-28 19:21:32 -0500

    Add files via upload

---
+++ ScheduleIt/account/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Evaluacion
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", 
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat Password", 
+                                widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            return forms.ValidationError('La contraseña no coincide.')
+        return cd['password2']
